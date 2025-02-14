7589ece254bcb99151852543d150daf6c08b179e
nemisis84 <simen.myrrusten@gmail.com>
2025-02-14 13:32:22 +0100

    Added ascon-suite

--- sensor-MCU/sensor-MCU.c
+++ sensor-MCU/sensor-MCU.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "ascon.h"
-#include "crypto_aead.h"
+#include "ascon/aead-masked.h"  // Masked ASCON AEAD version
 
-
-// Uncomment the desired encryption method
-// #define USE_ASCON_AEAD // Uses ascon_aead_encrypt / ascon_aead_decrypt
-#define USE_CRYPTO_AEAD // Uses crypto_aead_encrypt / crypto_aead_decrypt
-// #define USE_PROTECTED_BI32_ARMV6_LEVELED
-
+#define USE_ASCON_MASKED_AEAD // Uses masked ASCON AEAD for encryption/decryption
 
 void print_hex(const char *label, const uint8_t *data, size_t len) {
     printf("%s: ", label);
@@ -22,127 +16,54 @@ int main() {
     stdio_init_all();
     sleep_ms(3000);  // Wait for serial connection
 
-    printf("Starting ASCON encryption test...\n");
-
-    uint8_t key[CRYPTO_KEYBYTES] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-                                     0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
-
-    uint8_t nonce[CRYPTO_NPUBBYTES] = { 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7,
-                                        0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF };
+    printf("Starting ASCON Masked AEAD encryption test...\n");
 
     uint8_t plaintext[] = "Hello, ASCON!";
     uint64_t plaintext_len = sizeof(plaintext) - 1;
 
-    uint8_t ciphertext[plaintext_len + CRYPTO_ABYTES];  // Space for ciphertext + tag
-    uint64_t ciphertext_len;
+    uint8_t nonce[16] = { 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7,
+                           0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF };
 
-    uint8_t tag[CRYPTO_ABYTES];  // Authentication tag
     uint8_t associated_data[] = "MQTT-Test";
     uint64_t ad_len = sizeof(associated_data) - 1;
 
-#ifdef USE_ASCON_AEAD
-    printf("Using ascon_aead_encrypt()...\n");
+    uint8_t ciphertext[plaintext_len + 16]; // Ciphertext + tag
+    size_t ciphertext_len;
+
+    ascon_masked_key_128_t masked_key;
+    ascon128_masked_key_init(&masked_key, (uint8_t[]){ 0x00, 0x01, 0x02, 0x03,
+                                                       0x04, 0x05, 0x06, 0x07,
+                                                       0x08, 0x09, 0x0A, 0x0B,
+                                                       0x0C, 0x0D, 0x0E, 0x0F });
+
+#ifdef USE_ASCON_MASKED_AEAD
+    printf("Using ascon128_masked_aead_encrypt()...\n");
 
     // Encrypt
-    ascon_aead_encrypt(tag, ciphertext, plaintext, plaintext_len, 
-                       associated_data, ad_len, nonce, key);
+    ascon128_masked_aead_encrypt(ciphertext, &ciphertext_len, plaintext, plaintext_len, 
+                                 associated_data, ad_len, nonce, &masked_key);
 
     print_hex("Ciphertext", ciphertext, plaintext_len);
-    print_hex("Tag", tag, CRYPTO_ABYTES);
+    print_hex("Tag", ciphertext + plaintext_len, 16);
 
     // Decrypt
     uint8_t decrypted[plaintext_len];
-    int decrypt_result = ascon_aead_decrypt(decrypted, tag, ciphertext, 
-                                            plaintext_len, associated_data, 
-                                            ad_len, nonce, key);
+    size_t decrypted_len;
+
+    printf("Decrypting...\n");
+    int decrypt_result = ascon128_masked_aead_decrypt(decrypted, &decrypted_len, 
+                                                      ciphertext, ciphertext_len, 
+                                                      associated_data, ad_len, 
+                                                      nonce, &masked_key);
 
     if (decrypt_result != 0) {
         printf("Decryption failed!\n");
         return 1;
     }
 
-    printf("Decrypted message: %s\n", decrypted);
-
-#endif
-
-#ifdef USE_CRYPTO_AEAD
-    printf("Using crypto_aead_encrypt()...\n");
-
-    // Encrypt
-    crypto_aead_encrypt(ciphertext, &ciphertext_len, plaintext, plaintext_len, 
-                        associated_data, ad_len, NULL, nonce, key);
-
-    print_hex("Ciphertext", ciphertext, plaintext_len);
-    print_hex("Tag", ciphertext + plaintext_len, CRYPTO_ABYTES);
-
-    // Decrypt
-    uint8_t decrypted[plaintext_len];
-    uint64_t decrypted_len;
-
-    int decrypt_result = crypto_aead_decrypt(decrypted, &decrypted_len, NULL, 
-                                             ciphertext, ciphertext_len, 
-                                             associated_data, ad_len, nonce, key);
-
-    if (decrypt_result != 0) {
-        printf("Decryption failed!\n");
-        return 1;
-    }
-
+    printf("Decryption successful!\n");
     printf("Decrypted message: %s\n", decrypted);
 #endif
 
-// #ifdef USE_PROTECTED_BI32_ARMV6_LEVELED
-//     printf("Using protected_bi32_armv6_leveled()...\n");
-
-//     ascon_state_t state;
-//     mask_key_uint32_t masked_key;
-//     mask_npub_uint32_t masked_nonce;
-//     mask_m_uint32_t masked_plaintext;
-//     mask_c_uint32_t masked_ciphertext;
-
-//     generate_shares_encrypt(plaintext, &masked_plaintext, plaintext_len,
-//                             associated_data, NULL, ad_len,
-//                             nonce, &masked_nonce, key, &masked_key);
-
-//     ascon_initaead(&state, &masked_key, &masked_nonce);
-//     ascon_adata(&state, NULL, ad_len);
-//     ascon_encrypt(&state, &masked_ciphertext, &masked_plaintext, plaintext_len);
-//     ascon_final(&state, &masked_key);
-//     ascon_settag(&state, (mask_c_uint32_t*)tag);
-
-//     combine_shares_encrypt(&masked_ciphertext, ciphertext, plaintext_len + CRYPTO_ABYTES);
-
-//     print_hex("Ciphertext", ciphertext, plaintext_len);
-//     print_hex("Tag", tag, CRYPTO_ABYTES);
-
-//     // Decryption
-//     printf("Decrypting...\n");
-//     mask_m_uint32_t masked_decrypted;
-//     ascon_state_t state_dec;
-
-//     generate_shares_decrypt(ciphertext, &masked_ciphertext, ciphertext_len,
-//                             associated_data, NULL, ad_len,
-//                             nonce, &masked_nonce, key, &masked_key);
-
-//     ascon_initaead(&state_dec, &masked_key, &masked_nonce);
-//     ascon_adata(&state_dec, NULL, ad_len);
-//     ascon_decrypt(&state_dec, &masked_decrypted, &masked_ciphertext, plaintext_len);
-//     ascon_final(&state_dec, &masked_key);
-//     ascon_xortag(&state_dec, (mask_c_uint32_t*)tag);
-
-//     int auth_success = ascon_iszero(&state_dec);
-//     uint8_t decrypted[plaintext_len];
-//     combine_shares_decrypt(&masked_decrypted, decrypted, plaintext_len);
-
-//     if (auth_success == 0) {
-//         printf("Decryption successful!\n");
-//         printf("Decrypted message: %s\n", decrypted);
-//     } else {
-//         printf("Decryption failed: Authentication mismatch!\n");
-//         return 1;
-//     }
-
-// #endif
-
-//     return 0;
+    return 0;
 }
