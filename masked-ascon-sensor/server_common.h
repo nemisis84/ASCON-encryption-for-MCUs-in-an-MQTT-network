64fce433be8be51958f3f14ed1faa40373bdca8f
nemisis84 <simen.myrrusten@gmail.com>
2025-03-24 14:35:52 +0100

    Add six application options

--- masked-ascon-sensor/server_common.h
+++ masked-ascon-sensor/server_common.h
@@ -9,9 +9,8 @@
 #define SERVER_COMMON_H_
 
 #include "btstack.h"
-
+#include "experiment_settings.h"
 #define ADC_CHANNEL_TEMPSENSOR 4
-#define MAX_PACKETS 15
 
 extern int le_notification_enabled;
 extern hci_con_handle_t con_handle;
@@ -26,6 +25,10 @@ typedef struct {
 
 extern data_entry encryption_times[MAX_PACKETS];
 extern data_entry decryption_times[MAX_PACKETS];
+
+typedef struct {
+    uint16_t values[PAYLOAD_MULTIPLE];
+} temperatures;
 
 void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size);
 uint16_t att_read_callback(hci_con_handle_t connection_handle, uint16_t att_handle, uint16_t offset, uint8_t * buffer, uint16_t buffer_size);
